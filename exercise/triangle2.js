a0f2a42d9925408f86f792047bb5f2cb1a2c1f04
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-09-02 15:13:45 +0700

    exercise triangle

--- exercise/triangle2.js
+++ exercise/triangle2.js
@@ -1,9 +1,18 @@
 let triangle = (n) => {
-  // buat code nya di sini
+  let result = "";
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (j <= 9) {
+        result += `0${j} `;
+      } else {
+        result += `${j} `;
+      }
+    }
+    result += "\n";
+  }
+
+  console.log(result);
 };
 
-triangle(4);
-// 01
-// 01 02
-// 01 02 03
-// 01 02 03 04
+triangle(10);
