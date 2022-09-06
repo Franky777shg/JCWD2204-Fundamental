18d769f3b79d3b464746a3c2e034d0e0a12508e9
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-09-06 10:39:23 +0700

    intro to object

--- introToObject.js
+++ introToObject.js
@@ -84,3 +84,22 @@ let user4 = {
 console.log(user4.fullName);
 user4.fullName = "Alice Cooper";
 console.log(user4.fullName);
+console.log(Object.keys(user4));
+
+// descructuring assignment
+let a, b;
+[a, b] = [1, 2];
+console.log(a);
+console.log(b);
+
+// spread operator
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let finalArr = [...arr1, ...arr2];
+// console.log(finalArr)
+
+let finalObj = {
+  ...car,
+  ...person,
+};
+console.log(finalObj);
