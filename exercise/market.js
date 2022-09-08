a50db761df79b203ded056c0a680ca5bfe74ec24
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-09-08 14:49:13 +0700

    exercise market

---
+++ exercise/market.js
@@ -0,0 +1,45 @@
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+class Transaction {
+  cart = [];
+
+  addToCart(item, qty) {
+    // menambahkan property quantity ke dalam object product
+    item.quantity = qty;
+    // menmabahkan product ke dalam array cart
+    this.cart.push(item);
+  }
+
+  totalTrans() {
+    this.cart.forEach((item, index) => {
+      console.log(
+        `${index + 1}. ${item.name} x ${item.quantity} = ${
+          item.quantity * item.price
+        }`
+      );
+    });
+  }
+}
+
+let listProduct = [
+  new Product("Anggur", 12000),
+  new Product("Apel", 12000),
+  new Product("Pisang", 12000),
+  new Product("Jeruk", 12000),
+];
+
+// console.log(listProduct);
+
+let transaction = new Transaction();
+
+transaction.addToCart(listProduct[3], 3);
+transaction.addToCart(listProduct[0], 5);
+transaction.addToCart(listProduct[1], 5);
+// transaction.addToCart(listProduct[1], 5);
+transaction.totalTrans();
+// transaction.checkout(100000);
---
+++ exercise/review.js
@@ -0,0 +1,9 @@
+class Product {
+  name = "a";
+  price = 12;
+  name1 = "b";
+  price1 = 13;
+}
+
+let product = new Product();
+console.log(product);
