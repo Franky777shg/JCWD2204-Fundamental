a503fa6ecb7fbd6e3a91eef7b501ba9aa5ddbb80
Frengky Sihombing <64943858+Franky777shg@users.noreply.github.com>
2022-09-09 11:01:50 +0700

    hash table

--- set.js
+++ set.js
@@ -5,3 +5,6 @@ const setFruits = new Set(fruits);
 console.log(setFruits);
 setFruits.add("lemon");
 console.log(setFruits);
+console.log(setFruits.size);
+console.log(setFruits.has("kiwi"));
+console.log(setFruits.entries());
